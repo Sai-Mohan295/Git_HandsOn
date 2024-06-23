0e1198a7d7e7167404204bd70df1cdcfb525b7f9
sai <msai76433@gmail.com>
2024-06-23 20:35:09 +0530

    adding new files

---
+++ Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () =>{
+  return(
+    <center className="home-content">
+      <h1>Hi Hello</h1>
+      <h5>Click on USERTABLE to get table of users</h5>
+      <h5>Click on USERCARD to get card of users</h5>
+    </center>
+  )
+}
+
+export default Home;
